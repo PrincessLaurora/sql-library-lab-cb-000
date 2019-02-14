be0df221f94269ed38bdba27258879ad88441405
Laura Felce <crepu83@yahoo.it>
2019-02-14 14:20:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species) FROM series JOIN characters ON series.id = characters.id GROUP BY characters.species WHERE characters.species = 'human' ORDER BY characters.species;"
+  "SELECT series.title, COUNT(characters.species) FROM series JOIN characters ON series.id = characters.id WHERE characters.species = 'human' GROUP BY characters.species ORDER BY characters.species;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
